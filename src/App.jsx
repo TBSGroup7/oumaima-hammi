de1a32f6dee7344ebd52b76d9b645ee9d473da60
OumaimaHammi30 <hammi.oumaima01@gmail.com>
2024-02-12 13:32:06 +0100

    W3 Lists in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+/*import * as React from "react";
 const welcome={
   greeting:"hey",
   title:"react",
@@ -14,4 +14,50 @@ function App() {
     
   );
 }
-export default App;
+export default App;*/
+
+
+import * as React from "react";
+const list=[
+  {
+    title:"React",
+    url:"https://react.js.org/",
+    author:"Jordan Wakle",
+    num_comments:3,
+    points:4,
+    objectID:0,
+  },
+  {
+    title:"Redux",
+    url:"https://react.js.org/",
+    author:"Dan Abramov, Andrew Clark",
+    num_comments:2,
+    points:5,
+    objectID:1,
+  },
+];
+function App(){
+  return(
+    <div>
+      <h1> My hacker Stories</h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text"/>
+      <hr/>
+      <ul>
+        {list.map(function(item){
+          return ( 
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+          );
+        })}
+      </ul>
+    </div>
+    );
+}
+export default App
